6411827755ab5e73f904afae494180a5e208620a
alifyaratuzahra <alifyaratuzahra5@gmail.com>
2025-02-22 14:27:43 +0700

    project-fix-update-01

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'MyProduct'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Belanja murah dan menyenangkan.'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'Aplikasi belanja online mengguunakan Next.js and MongoDB'
